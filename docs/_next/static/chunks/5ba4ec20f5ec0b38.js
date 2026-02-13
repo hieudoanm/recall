8f9caedb64862db6e4c9c982b3bcf56e60126554
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-02-13 18:19:57 +0700

    update capacitor.config.ts

--- docs/_next/static/chunks/5ba4ec20f5ec0b38.js
+++ docs/_next/static/chunks/5ba4ec20f5ec0b38.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/af20ddb5af60666f.js",
   "static/chunks/87d298d7d8de71bc.js",
   "static/chunks/e2b3d74db7ba8b5b.js",
-  "static/chunks/57882d5257d9b26b.css",
-  "static/chunks/turbopack-b1cb99dc8a9e0831.js"
+  "static/chunks/eec72052ab50c491.css",
+  "static/chunks/turbopack-47f6fdd7becf9633.js"
 ])
